f7c59b2b34decd29ac31194e334e09b06a5ee838
Auro Tripathy <ipserv@yahoo.com>
2020-04-11 15:50:52 -0700

    added height and width are paramters

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 # HIP_VISIBLE_DEVICES=0,1,2,3 python3.6  micro_benchmarking_pytorch.py --dataparallel --network resnet50 --iterations 20 --batch-size 128
 # HIP_VISIBLE_DEVICES=0,1,2,3 python3.6  micro_benchmarking_pytorch.py --dataparallel --network resnet50 --iterations 20 --batch-size 256
 # HIP_VISIBLE_DEVICES=0,1,2,3 python3.6  micro_benchmarking_pytorch.py --dataparallel --network resnet50 --iterations 20 --batch-size 512
-HIP_VISIBLE_DEVICES=0,1,2,3 python3.6  micro_benchmarking_pytorch.py --dataparallel --network resnet50 --iterations 20 --batch-size 1024
-
+CUDA_VISIBLE_DEVICES=0 python  micro_benchmarking_pytorch.py --dataparallel --network resnet50 --image-height 224 --image-width 224 --iterations 20 --batch-size 16
 
